e60b45de4ee42f058940b93bf5c3e65358b04b31
vasquesgabriel91 <vasquesgabriel91@gmail.com>
2025-05-16 11:34:13 -0300

    feature: add middleware

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -8,15 +8,14 @@ const router = express.Router();
 // Create User Admin/Doctor
 
 router.post("/admin", AdminController.createAdmin);//ok
-router.get("/admin/:id", AdminController.getAdmById);//ok
-router.post("/admin/listAll", AdminController.createAdmin);//ok
-router.post("/admin/update/:id", AdminController.updateAdmin);//ok
-router.delete("/admin/delete/:id", AdminController.delete);//ok
+router.get("/admin/:id",  Auth.verifyToken, authorize(["admin"]), AdminController.getAdmById);//ok
+router.post("/admin/update/:id",  Auth.verifyToken, authorize(["admin"]), AdminController.updateAdmin);//ok
+router.delete("/admin/delete/:id", Auth.verifyToken, authorize(["admin"]), AdminController.delete);//ok
 
-router.get("/admin/appointments/:id", Auth.verifyToken, AdminController.getAppointmentsById); //ok
-router.post("/admin/appointments", Auth.verifyToken, AdminController.createAppointments); //ok
-router.get('/appointments/search', Auth.verifyToken, AdminController.searchAppointments);//ok
-router.put("/admin/appointments/update/:id", Auth.verifyToken, AdminController.updateAppointments); 
-router.delete("/admin/appointments/delete/:id", Auth.verifyToken, AdminController.deleteAppointments);//ok
+router.get("/admin/appointments/:id", Auth.verifyToken, authorize(["admin"]), AdminController.getAppointmentsById); //ok
+router.post("/admin/appointments", Auth.verifyToken, authorize(["admin"]), AdminController.createAppointments); //ok
+router.get('/appointments/search', Auth.verifyToken, authorize(["admin"]), AdminController.searchAppointments);//ok
+router.put("/admin/appointments/update/:id", Auth.verifyToken, authorize(["admin"]), AdminController.updateAppointments); 
+router.delete("/admin/appointments/delete/:id", Auth.verifyToken, authorize(["admin"]), AdminController.deleteAppointments);//ok
 
 module.exports = router;
